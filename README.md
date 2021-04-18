72fd8436002c66931c2076e3ac27c977fc1283e8
Jahid Joni <jh.joni07@gmail.com>
2021-04-18 12:20:22 +0600

    done

---
+++ README.md
@@ -0,0 +1,19 @@
+# CarSevice-website-server-JahidJoni
+
+This project is build by node js.
+
+# Live Hosted site: 
+
+Database used: Mongodb
+
+Framework: Express.js
+
+Other Services used: Body-Parser, Cors, Nodemon, DotEnv, ObjectId
+
+Data Collection stored:
+1. Orders by user
+2. Reviews by user
+3. Add Service by admin
+4. Add Admin by Admin
+
+Project Deployed in Heroku.
--- index.js
+++ index.js
@@ -23,69 +23,87 @@ client.connect(err => {
   const serviceCollection = client.db("car-service").collection("services");
   const reviewCollection = client.db("car-service").collection("review");
   const adminCollection = client.db("car-service").collection("admin");
-  
-  app.post('/addAppointment', (req, res)=>{
+
+  app.post('/addAppointment', (req, res) => {
     const appointment = req.body;
     appointmentsCollection.insertOne(appointment)
-    .then(result=>{
-      res.status(200).json(result.insertedCount)
-    })
+      .then(result => {
+        res.status(200).json(result.insertedCount)
+      })
   })
 
-  app.get('/services', (req, res)=>{
+  app.get('/services', (req, res) => {
     serviceCollection.find()
-    .toArray((err, items)=>{
-      res.send(items)
-    })
+      .toArray((err, items) => {
+        res.send(items)
+      })
   })
 
-  app.get('/services/:id', (req, res)=>{
-    serviceCollection.find({_id: ObjectId(req.params.id)})
-    .toArray((err, documents)=>{
-      res.send(documents[0]);
-    })
+  app.get('/services/:id', (req, res) => {
+    serviceCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      })
   })
 
-  app.post('/addService', (req, res)=>{
+  app.post('/addService', (req, res) => {
     const newService = req.body;
     serviceCollection.insertOne(newService)
-    .then(result=>{
-      console.log('count', result.insertedCount)
-      res.send(result.insertedCount >0 )
-    })
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
   })
 
-  app.post('/addReviews', (req, res)=>{
+  app.post('/addReviews', (req, res) => {
     const newReview = req.body;
     reviewCollection.insertOne(newReview)
-    .then(result=>{
-      console.log('count', result.insertedCount)
-      res.send(result.insertedCount >0 )
-    })
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
   })
 
-  app.get('/reviews', (req, res)=>{
+  app.get('/reviews', (req, res) => {
     reviewCollection.find()
-    .toArray((err, items)=>{
-      res.send(items)
-    })
+      .toArray((err, items) => {
+        res.send(items)
+      })
   })
 
-  app.get('/orders', (req, res)=>{
+  app.get('/orders', (req, res) => {
     const email = req.query.email;
-    appointmentsCollection.find({email: req.query.email})
-    .toArray((err, documents)=>{
-      res.send(documents);
-    })
+
+    adminCollection.find({ email: email })
+      .toArray((err, documents) => {
+        if (documents.length)
+          appointmentsCollection.find()
+            .toArray((err, items) => {
+              res.send(items)
+            })
+        if (documents.length === 0) {
+          appointmentsCollection.find({ email: req.query.email })
+            .toArray((err, documents) => {
+              res.send(documents);
+            })
+        }
+      })
   })
 
-  app.post('/addAdmin', (req, res)=>{
+  app.post('/addAdmin', (req, res) => {
     const newAdmin = req.body;
     adminCollection.insertOne(newAdmin)
-    .then(result=>{
-      console.log('Admin count', result.insertedCount)
-      res.send(result.insertedCount >0 )
-    })
+      .then(result => {
+        console.log('Admin count', result.insertedCount)
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+  app.post('/isAdmin', (req, res) => {
+    const email = req.body.email
+    adminCollection.find({ email: email })
+      .toArray((err, admin) => {
+        res.send(admin.length > 0);
+      })
+
   })
 
 });
